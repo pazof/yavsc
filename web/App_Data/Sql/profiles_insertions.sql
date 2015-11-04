8c5de206e1ee4478f078eb3161ede3c05c187ebc
Paul Schneider <paul@pschneider.fr>
2015-11-04 20:08:48 +0100

    Fixes missing files

---
+++ web/App_Data/Sql/profiles_insertions.sql
@@ -0,0 +1,21 @@
+﻿INSERT INTO profiles (username,applicationname,isanonymous,lastactivitydate,lastupdateddate) 
+SELECT users.username , users.applicationname, FALSE, 
+  users.lastactivitydate, now()  
+  FROM users LEFT OUTER JOIN profiles ON (users.username = profiles.username 
+   AND users.applicationname = profiles.applicationname)
+    where profiles.username IS NULL;
+    
+ALTER TABLE users
+  ADD FOREIGN KEY (applicationname, username) 
+  REFERENCES profiles (applicationname, username) ON UPDATE CASCADE ON DELETE CASCADE;
+  
+ALTER TABLE profiles
+DROP CONSTRAINT fk_profileusers;
+
+update profiles SET isanonymous = FALSE where isanonymous IS NULL;
+
+ALTER TABLE profiles
+   ALTER COLUMN isanonymous SET DEFAULT TRUE;
+ALTER TABLE profiles
+   ALTER COLUMN isanonymous SET NOT NULL;
+
---
+++ web/Helpers/YavscAjaxHelper.cs
@@ -0,0 +1,64 @@
+﻿//
+//  YavscAjaxHelper.cs
+//
+//  Author:
+//       Paul Schneider <paul@pschneider.fr>
+//
+//  Copyright (c) 2015 GNU GPL
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Lesser General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+using System;
+using System.Web.Mvc;
+using System.Collections.Generic;
+using Yavsc.Model.Messaging;
+
+namespace Yavsc.Helpers
+{
+	/// <summary>
+	/// Yavsc ajax helper.
+	/// </summary>
+	public static class YavscAjaxHelper
+	{
+		/// <summary>
+		/// Notify the specified helper, message and click_action.
+		/// </summary>
+		/// <param name="helper">Helper.</param>
+		/// <param name="message">Message.</param>
+		/// <param name="click_action">Click action.</param>
+		public static void Notify(this AjaxHelper helper, string message, string click_action=null) {
+			
+			if (helper.ViewData ["Notifications"] == null)
+				helper.ViewData ["Notifications"] = new List<Notification> ();
+			(helper.ViewData ["Notifications"] as List<Notification>).Add (
+				new Notification { body = QuoteJavascriptString(message), 
+					click_action = click_action } ) ;
+		}
+
+		/// <summary>
+		/// Quotes the javascript string.
+		/// </summary>
+		/// <returns>The javascript string.</returns>
+		/// <param name="str">String.</param>
+		public static string QuoteJavascriptString(string str)
+		{
+			str = str.Replace ("\n", "\\n");
+			if (str.Contains ("'"))
+			if (str.Contains ("\""))
+				return "'" + str.Replace ("'", "\\'") + "'";
+			else
+				return "\"" + str + "\"";
+			return "'" + str + "'";
+		}
+	}
+}
