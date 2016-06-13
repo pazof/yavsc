28ef3313afdcb6403d40854cad0450f03d0e5b90
Paul Schneider <paul@pschneider.fr>
2016-06-13 14:31:14 +0200

    casse mniuscule pour les champ de l'API

--- Yavsc/ViewModels/Account/Me.cs
+++ Yavsc/ViewModels/Account/Me.cs
@@ -1,20 +1,24 @@
+using System.Linq;
+
 namespace  Yavsc.Models.Auth
 {
     public class Me {
         public Me(ApplicationUser user)
         {
-           Id = user.Id;
-           GivenName = user.UserName;
-           Emails = new string [] { user.Email } ;
+           id = user.Id;
+           givenName = user.UserName;
+           emails = new string [] { user.Email } ;
+           roles = user.Roles.Select(r=>r.RoleId).ToArray();
         }
-        public string Id { get;  set; }
-        public string GivenName { get; set; }
-        public string[] Emails { get; set; }
+        public string id { get;  set; }
+        public string givenName { get; set; }
+        public string[] emails { get; set; }
+        public string[] roles { get; set; }
         /// <summary>
         /// Known as profile, could point to an avatar
         /// </summary>
         /// <returns></returns>
-        public string Url { get; set; }
+        public string url { get; set; }
         
     }
 
