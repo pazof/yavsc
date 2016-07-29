394ed3439987733e3ae0aa24cb0c22124e2e5927
Paul Schneider <paul@pschneider.fr>
2016-07-29 15:49:30 +0200

    fixe le premier démarrage

--- Yavsc/Startup/Startup.FileServer.cs
+++ Yavsc/Startup/Startup.FileServer.cs
@@ -14,16 +14,20 @@ namespace Yavsc
         public void ConfigureFileServerApp(IApplicationBuilder app,
                 SiteSettings siteSettings, IHostingEnvironment env)
         {
-            app.UseFileServer(new FileServerOptions()
-            {
-                FileProvider = new PhysicalFileProvider(
-                    Path.Combine(
+            var rootPath = Path.Combine(
                         env.WebRootPath,
                         // TODO: add a ressource serveur id here, 
                         // or remove the blog entry id usage, to use the userid instead
                         // and an user defined optional subdir.
                         siteSettings.UserFiles.DirName
-                    )),
+                    );
+            var rootInfo = new DirectoryInfo(rootPath);
+            if (!rootInfo.Exists) rootInfo.Create();
+
+
+            app.UseFileServer(new FileServerOptions()
+            {
+                FileProvider = new PhysicalFileProvider(rootPath),
                 RequestPath = new PathString("/" + siteSettings.UserFiles.DirName),
                 EnableDirectoryBrowsing = false
             });
