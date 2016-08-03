15dd53007c12187f00c34566aab0fc223f2324d9
Paul Schneider <paul@pschneider.fr>
2016-08-03 16:38:25 +0200

    markdown strike & underline & htbase

--- Yavsc/Settings/ThirdPartyFiles.cs
+++ Yavsc/Settings/ThirdPartyFiles.cs
@@ -4,5 +4,8 @@ namespace  Yavsc
         public class ThirdPartyFiles {
             public string DirName { get; set; }
             public string Quota { get; set; }
+            public string Blog { get; set; }
+            public string Bills { get; set; }
+
         }
 }
