00232a4b033042212dec65d546e98d1b0ad6432d
Paul Schneider <paul@pschneider.fr>
2016-08-30 23:04:25 +0200

    refactoring

---
+++ Yavsc/Model/Market/Money.cs
@@ -0,0 +1,37 @@
+using System;
+
+namespace Yavsc.Models.Market {
+ public class Money : IUnit<decimal>
+    {
+        public Money(string name, decimal euroExchangeRate)
+        {
+            Name = name;
+            EuroExchangeRate = euroExchangeRate;
+        }
+
+        public string Name
+        {
+            get; private set ;
+        }
+
+        public decimal EuroExchangeRate
+        {
+             get; private set ;
+        }
+        public bool CanConvertFrom(IUnit<decimal> other)
+        {
+            if (other is Money)
+                  return true;
+            return false;
+        }
+
+        public decimal ConvertFrom(IUnit<decimal> other, decimal orgValue)
+        {
+            if (other is Money) {
+                var om = other as Money;
+                return orgValue * om.EuroExchangeRate / EuroExchangeRate;
+            }
+            throw new NotImplementedException();
+        }
+    }
+ }
