c5274146e34b86e4ba7daaa2e51660a2d40684ac
Paul Schneider <paul@pschneider.fr>
2016-08-30 23:04:25 +0200

    refactoring

--- Yavsc/Model/Market/Service.cs
+++ Yavsc/Model/Market/Service.cs
@@ -1,52 +1,7 @@
 
 namespace Yavsc.Models.Market {
-    using System;
+    using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations.Schema;
-
-    public enum BillingMode { 
-        Unitary,
-        SetPrice,
-        ByExecutionTime
-    }
-
-    public interface IUnit<VType> {
-          string Name { get; }
-          bool CanConvertFrom(IUnit<VType> other);
-         VType ConvertFrom (IUnit<VType> other, VType orgValue) ;
-    }
-    public class Money : IUnit<decimal>
-    {
-        public Money(string name, decimal euroExchangeRate)
-        {
-            Name = name;
-            EuroExchangeRate = euroExchangeRate;
-        }
-
-        public string Name
-        {
-            get; private set ;
-        }
-
-        public decimal EuroExchangeRate
-        {
-             get; private set ;
-        }
-        public bool CanConvertFrom(IUnit<decimal> other)
-        {
-            if (other is Money)
-                  return true;
-            return false;
-        }
-
-        public decimal ConvertFrom(IUnit<decimal> other, decimal orgValue)
-        {
-            if (other is Money) {
-                var om = other as Money;
-                return orgValue * om.EuroExchangeRate / EuroExchangeRate;
-            }
-            throw new NotImplementedException();
-        }
-    }
 
     public partial class Service : BaseProduct
     {
@@ -54,13 +9,7 @@ namespace Yavsc.Models.Market {
         [ForeignKey("ContextId")]
         public virtual Activity Context { get; set; }
 
-        public BillingMode? Billing { get; set; }
-        // TODO public ServiceSpecification Specification { get; set; }
-        /// <summary>
-        /// In euro
-        /// </summary>
-        /// <returns></returns>
-        public decimal? Pricing { get; set; }
+        public List<IBillingClause> Billing { get; set; }
         
     }
 
