1af6883c6080bd189efe0f793c83a31e5200ac06
Paul Schneider <paul@pschneider.fr>
2016-09-01 18:42:12 +0200

    Billing clauses still are too virtual to be mapped in db (no default implementation yet)

--- Yavsc/Model/Market/Service.cs
+++ Yavsc/Model/Market/Service.cs
@@ -9,6 +9,19 @@ namespace Yavsc.Models.Market {
         [ForeignKey("ContextId")]
         public virtual Activity Context { get; set; }
 
+        /// <summary>
+        /// List of billing clause,
+        /// associated to this service, 
+        /// that defines the transformation rules
+        /// to take in account during the transformation
+        /// of a corresponding prestation to an amount to pay.
+        /// This property is built at constructing a new instance
+        /// and is not mapped in database. 
+        /// For the moment, it's hard coded only.
+        /// </summary>
+        /// <returns></returns>
+
+        [NotMapped]
         public List<IBillingClause> Billing { get; set; }
         
     }
