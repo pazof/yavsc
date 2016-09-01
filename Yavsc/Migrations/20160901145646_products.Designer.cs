1deec89137aae587a7d2329c515d5ab663ceef73
Paul Schneider <paul@pschneider.fr>
2016-09-01 18:39:13 +0200

    references services and products in db context

--- Yavsc/Migrations/20160901145646_products.Designer.cs
+++ Yavsc/Migrations/20160901145646_products.Designer.cs
@@ -8,9 +8,10 @@ using Yavsc.Models;
 namespace Yavsc.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20160901145646_products")]
+    partial class products
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "7.0.0-rc1-16348");
