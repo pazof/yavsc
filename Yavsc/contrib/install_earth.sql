4475dca3fa01b408c22ea26d044c4d9994a12fe4
Paul Schneider <paul@pschneider.fr>
2016-09-17 03:33:07 +0200

    install a basic earth distance computing as an Npgsql extension

---
+++ Yavsc/contrib/install_earth.sql
@@ -0,0 +1,6 @@
+﻿SELECT version();
+CREATE EXTENSION "cube"; -- you will not be able install "earthdistance" w/o "cube" extension
+CREATE EXTENSION "earthdistance"; --or any other extension you need
+create extension earthdistance;
+select earth();
+select earth_distance(ll_to_earth(12.4,2.0),ll_to_earth(12.456,2.8043)); -- 87km
