5a38ce23e52db6ee2c13560eecefd673bd6f4053
Paul Schneider <paul@pschneider.fr>
2016-09-18 00:13:37 +0200

    cleaning

--- BookAStar/BookAStar/Model/Workflow/Estimate.cs
+++ BookAStar/BookAStar/Model/Workflow/Estimate.cs
@@ -1,23 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BookAStar.Model.Workflow
 {
     public partial class Estimate
     {
         public long Id { get; set; }
-
+        
         public long? CommandId { get; set; }
-        /// <summary>
-        /// A command is not required to create
-        /// an estimate,
-        /// it will result in a new estimate template
-        /// </summary>
-        /// <returns></returns>
-        public BookQueryPage Query { get; set; }
         public string Description { get; set; }
         public int? Status { get; set; }
         public string Title { get; set; }
@@ -51,7 +41,6 @@ namespace BookAStar.Model.Workflow
         }
         
         public string OwnerId { get; set; }
-        private RemoteEntity<BookQueryData, long> BookQueries;
         
         public string ClientId { get; set; }
     }
