21a40cdb5540327d133d899c688c087c9ea09245
Paul Schneider <paul@pschneider.fr>
2016-09-18 00:22:30 +0200

    fixes the bookquery entity

--- BookAStar/BookAStar/DataManager.cs
+++ BookAStar/BookAStar/DataManager.cs
@@ -12,17 +12,24 @@ namespace BookAStar
 
     public class DataManager
     {
+        // TODO userinfo estimatetemplate rating service product tag   
         public RemoteEntity<BookQueryData, long> BookQueries { get; set; }
         public RemoteEntity<Estimate, long> Estimates { get; set; }
         public RemoteEntity<Blog, long> Blogspot { get; set; }
-
+        public static DataManager Current
+        {
+            get
+            {
+                return App.CurrentApp.DataManager;
+            }
+        }
         public DataManager()
         {
-            BookQueries = new RemoteEntity<BookQueryData, long>("bookqueries",
-                q => q.CommandId);
-            Estimates = new RemoteEntity<Estimate, long>("estimates",
+            BookQueries = new RemoteEntity<BookQueryData, long>("bookquery",
+                q => q.Id);
+            Estimates = new RemoteEntity<Estimate, long>("estimate",
                 x => x.Id);
-            Blogspot = new RemoteEntity<Blog, long>("blogspot", 
+            Blogspot = new RemoteEntity<Blog, long>("blog", 
                 x=>x.Id);
         }
         public async Task<BookQueryData> GetBookQuery(long bookQueryId)
