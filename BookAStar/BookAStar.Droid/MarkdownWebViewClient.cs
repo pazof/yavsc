d5b401fc23e21673182a494058b5d2729723ca8a
Paul Schneider <paul@pschneider.fr>
2016-09-18 00:08:39 +0200

    allows a form method=get action="file:///validate?md=text"

---
+++ BookAStar/BookAStar.Droid/MarkdownWebViewClient.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.Webkit;
+using Java.Lang;
+using Java.IO;
+using Android.Content.Res;
+
+namespace BookAStar.Droid
+{
+    class MarkdownWebViewClient : WebViewClient
+    {
+        Action<string> update;
+        public MarkdownWebViewClient(Action<string> update) : base()
+        {
+            this.update = update;
+        }
+        private static Activity getActivity ()
+        {
+            return (Activity)App.PlateformSpecificInstance;
+        }
+        public string Markdown { get; private set; }
+        public override WebResourceResponse ShouldInterceptRequest(WebView view, IWebResourceRequest request)
+        {
+            if (request.Url.Scheme=="file")
+            {
+                if (request.Url.Path=="/android_asset/validate")
+                {
+                    Markdown = request.Url.GetQueryParameter("md");
+                    update(Markdown);
+                    return new WebResourceResponse("application/json", "utf-8" ,200, "Ok", null, null);
+                }
+            }
+            return base.ShouldInterceptRequest(view, request);
+        }
+    }
+}
