4017e9ccb8186bc4d58cef244136018bdf1beaa8
Paul Schneider <paul@pschneider.fr>
2016-09-23 12:29:50 +0200

    implements an avatar

--- Yavsc/Model/Identity/ApplicationUser.cs
+++ Yavsc/Model/Identity/ApplicationUser.cs
@@ -9,24 +9,62 @@ namespace Yavsc.Models
 {
     public class ApplicationUser : IdentityUser
     {
-
-        [Display(Name="AccountBalance")]
+        /// <summary>
+        /// Another me, as a byte array.
+        /// This value points a picture that may be used 
+        /// to sign documents.
+        /// </summary>
+        /// <returns>the path to an user's image, relative to it's dir<summary>
+        /// <see>Startup.UserFilesOptions</see>
+        /// </summary>
+        /// <returns></returns>
+        public string Avatar { get; set; }
+        /// <summary>
+        /// WIP Paypal
+        /// </summary>
+        /// <returns></returns>
+        [Display(Name="Account balance")]
         public virtual AccountBalance AccountBalance { get; set; }
 
+        /// <summary>
+        /// User's posts
+        /// </summary>
+        /// <returns></returns>
         [InverseProperty("Author")]
         public virtual List<Blog> Posts { get; set; }
 
+        /// <summary>
+        /// User's contact list
+        /// </summary>
+        /// <returns></returns>
         [InverseProperty("Owner")]
         public virtual List<Contact> Book { get; set; }
 
+        /// <summary>
+        /// External devices using the API
+        /// </summary>
+        /// <returns></returns>
         [InverseProperty("DeviceOwner")]
         public virtual List<GoogleCloudMobileDeclaration> Devices { get; set; }
         
+        /// <summary>
+        /// User's circles
+        /// </summary>
+        /// <returns></returns>
         [InverseProperty("Owner")]
 
         public virtual List<Circle> Circles { get; set; }
+
+        /// <summary>
+        /// Billing postal address
+        /// </summary>
+        /// <returns></returns>
         public virtual Location PostalAddress { get; set; }
 
+        /// <summary>
+        /// User's Google calendar
+        /// </summary>
+        /// <returns></returns>
         public string DedicatedGoogleCalendar { get; set; }
 
         public override string ToString() {
