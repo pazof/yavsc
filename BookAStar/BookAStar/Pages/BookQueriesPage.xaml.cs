5d030bf51f7a3d69a6a9e931e327cf24bfdd5412
Paul Schneider <paul@pschneider.fr>
2016-10-02 19:33:31 +0200

    cleaning thanks to Xaml code

--- BookAStar/BookAStar/Pages/BookQueriesPage.xaml.cs
+++ BookAStar/BookAStar/Pages/BookQueriesPage.xaml.cs
@@ -1,44 +1,18 @@
-﻿using BookAStar.Helpers;
-using BookAStar.Model;
-using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Input;
+﻿using BookAStar.Model;
+using BookAStar.ViewModels;
 using Xamarin.Forms;
 using XLabs.Ioc;
 using XLabs.Platform.Services;
 
 namespace BookAStar.Pages
 {
+
     public partial class BookQueriesPage : ContentPage
     {
         public BookQueriesPage()
         {
             InitializeComponent();
-
-            BindingContext = DataManager.Current.BookQueries;
-        }
-        protected override void OnBindingContextChanged()
-        {
-            list.ItemsSource = BindingContext as ObservableCollection<BookQueryData>;
-            base.OnBindingContextChanged();
-        }
-
-        public RemoteEntity<BookQueryData,long> Queries
-        {
-            get
-            {
-                return BindingContext!=null? BindingContext as RemoteEntity<BookQueryData, long>:null;
-            }
-        }
-
-        protected override void OnAppearing()
-        {
-            base.OnAppearing();
-            if (Queries.CanExecute(null)) Queries.Execute(null);
+            BindingContext = new BookQueriesViewModel();
         }
 
         private void OnViewDetail(object sender, ItemTappedEventArgs e)
