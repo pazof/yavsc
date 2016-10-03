e0b774b67e24956c769974481bb7fe2fb6207af7
Paul Schneider <paul@pschneider.fr>
2016-10-03 18:06:52 +0200

    implements some internal web exception

---
+++ BookAStar/BookAStar/Helpers/NotIdentifiedException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace BookAStar
+{
+    public class NotIdentifiedException : Exception
+    {
+        public NotIdentifiedException(string message) : base(message) { }
+    }
+}
+
