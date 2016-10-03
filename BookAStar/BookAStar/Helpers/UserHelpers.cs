eabc53b3b2a12b7084ff65b7f10ff33edd59a825
Paul Schneider <paul@pschneider.fr>
2016-10-03 20:15:04 +0200

    cleanning

--- BookAStar/BookAStar/Helpers/UserHelpers.cs
+++ BookAStar/BookAStar/Helpers/UserHelpers.cs
@@ -1,12 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
 using System.Globalization;
 using System.IO;
-using System.Linq;
 using System.Net.Http;
-using System.Text;
-using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 
