f42b66383aec40cc987ee6902b628889fbc4f160
Paul Schneider <paul@pschneider.fr>
2016-10-04 02:44:44 +0200

    refactoring

--- BookAStar/BookAStar/Helpers/LocaLEntity.cs
+++ BookAStar/BookAStar/Helpers/LocaLEntity.cs
@@ -1,19 +1,23 @@
 ﻿
+using BookAStar.Interfaces;
 using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 
 namespace BookAStar
 {
-    public class LocalEntity<V, K> : ObservableCollection<V> where K : IEquatable<K>
+    public class LocalEntity<V, K> : ObservableCollection<V>, ILocalEntity<V, K> where K : IEquatable<K>
     {
         public V CurrentItem { get; protected set; }
-        protected Func<V, K> GetKey { get; set; }
+        public Func<V, K> GetKey { get; set; }
         public LocalEntity(Func<V, K> getKey) : base()
         {
             if (getKey == null) throw new InvalidOperationException();
             GetKey = getKey;
+            IList<V> l = this;
         }
+
         public virtual void Merge(V item)
         {
             var key = GetKey(item);
@@ -28,6 +32,11 @@ namespace BookAStar
         {
             CurrentItem = this.Single(x => GetKey(x).Equals(key));
             return CurrentItem;
+        }
+
+        public void Load()
+        {
+            this.Populate<V>();
         }
     }
 
---
+++ BookAStar/BookAStar/Interfaces/ILocalEntity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BookAStar
+{
+    public interface ILoadable
+    {
+        void Load();
+    }
+
+    public interface ILocalEntity<V, K> : ILoadable where K : IEquatable<K>
+    {
+        V CurrentItem { get; }
+
+        Func<V, K> GetKey { get; set; }
+
+        V LocalGet(K key);
+
+        void Merge(V item);
+
+    }
+}
