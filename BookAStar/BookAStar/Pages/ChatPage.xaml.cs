c88fc39a291929315656765e6a21ec4968d976b9
Paul Schneider <paul@pschneider.fr>
2016-10-10 12:58:14 +0200

    A Chat window

---
+++ BookAStar/BookAStar/Pages/ChatPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNet.SignalR.Client;
+using System;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using Xamarin.Forms;
+
+namespace BookAStar.Pages
+{
+    public partial class ChatPage : ContentPage
+    {
+        public ObservableCollection<string> Messages { get; set; }
+        public string ChatUser { get; set; }
+
+        private HubConnection chatHubConnection;
+        private IHubProxy chatHubProxy;
+
+        public ChatPage()
+        {
+            InitializeComponent();
+
+            Title = "Chat";
+
+            sendButton.Clicked += async (sender, args) => {
+                IsBusy = true;
+
+                try
+                {
+                    await chatHubProxy.Invoke<string>("Send", ChatUser, messageEntry.Text);
+                }
+                catch (Exception ex)
+                {
+
+                    Debug.WriteLine(ex);
+                }
+
+                IsBusy = false;
+            };
+             messageList.ItemsSource = Messages = new ObservableCollection<string>();
+        }
+
+        protected override async void OnAppearing()
+        {
+            IsBusy = true;
+            chatHubConnection = new HubConnection(Constants.SignalRHubsUrl);
+            chatHubProxy = chatHubConnection.CreateHubProxy("ChatHub");
+            chatHubProxy.On<string, string>("AddMessage", (n, m) => {
+                Messages.Add(string.Format("{0} says: {1}", n, m));
+            });
+            await chatHubConnection.Start();
+            IsBusy = false;
+            sendButton.IsEnabled = true;
+        }
+    }
+}
---
+++ BookAStar/BookAStar/ViewModels/ChatViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XLabs.Forms.Mvvm;
+
+namespace BookAStar.ViewModels
+{
+    class ChatViewModel: ViewModel
+    {
+        public string UserName {
+            get; set;
+        }
+    }
+}
