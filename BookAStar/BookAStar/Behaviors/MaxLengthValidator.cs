2b02d8c01a99d48826bd056f6fb506f8b6efba5b
Paul Schneider <paul@pschneider.fr>
2016-10-12 03:54:53 +0200

    gives the billing line description a maximum length

--- BookAStar/BookAStar/Behaviors/MaxLengthValidator.cs
+++ BookAStar/BookAStar/Behaviors/MaxLengthValidator.cs
@@ -7,6 +7,35 @@ using Xamarin.Forms;
 
 namespace BookAStar.Behaviors
 {
+    public class EditorMaxLengthValidator : Behavior<Editor>
+    {
+        public static readonly BindableProperty MaxLengthProperty = BindableProperty.Create("MaxLength", typeof(int), typeof(MaxLengthValidator), 0);
+
+        public int MaxLength
+        {
+            get { return (int)GetValue(MaxLengthProperty); }
+            set { SetValue(MaxLengthProperty, value); }
+        }
+
+        protected override void OnAttachedTo(Editor bindable)
+        {
+            bindable.TextChanged += bindable_TextChanged;
+        }
+
+        public bool IsValid { get; set; }
+
+        private void bindable_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            IsValid = e.NewTextValue.Length <= 0 || e.NewTextValue.Length <= MaxLength;
+            if (!IsValid && e.NewTextValue.Length > MaxLength)
+                ((Editor)sender).Text = e.NewTextValue.Substring(0, MaxLength);
+        }
+
+        protected override void OnDetachingFrom(Editor bindable)
+        {
+            bindable.TextChanged -= bindable_TextChanged;
+        }
+    }
     public class MaxLengthValidator : Behavior<Entry>
     {
         public static readonly BindableProperty MaxLengthProperty = BindableProperty.Create("MaxLength", typeof(int), typeof(MaxLengthValidator), 0);
