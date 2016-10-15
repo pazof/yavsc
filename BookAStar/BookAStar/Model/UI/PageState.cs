137b5430010b4a6463b81a84f69b957c53ba40b9
Paul Schneider <paul@pschneider.fr>
2016-10-15 16:09:55 +0200

    a better layout, + start of app state imple

---
+++ BookAStar/BookAStar/Model/UI/PageState.cs
@@ -0,0 +1,10 @@
+﻿namespace BookAStar.Model.UI
+{
+    internal class PageState
+    {
+        internal int Position { get; set; }
+        internal object BindingContext { get; set; }
+        internal string PageType { get; set; }
+    }
+}
+
