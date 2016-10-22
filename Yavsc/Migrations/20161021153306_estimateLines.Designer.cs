690a5f8ee461a32bcc7d9597ca2bf45d53bbdbb1
Paul Schneider <paul@pschneider.fr>
2016-10-23 01:31:09 +0200

    .

--- Yavsc/Migrations/20161021153306_estimateLines.Designer.cs
+++ Yavsc/Migrations/20161021153306_estimateLines.Designer.cs
@@ -8,9 +8,10 @@ using Yavsc.Models;
 namespace Yavsc.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20161021153306_estimateLines")]
+    partial class estimateLines
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "7.0.0-rc1-16348");
--- Yavsc/Model/Billing/CommandLine.cs
+++ Yavsc/Model/Billing/CommandLine.cs
@@ -1,6 +1,7 @@
 
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using Newtonsoft.Json;
 using Yavsc.Models.Market;
 
 namespace Yavsc.Models.Billing
@@ -11,11 +12,16 @@ namespace Yavsc.Models.Billing
      [Key(), DatabaseGenerated(DatabaseGeneratedOption.Identity)]
      public long Id { get; set; }
      
-     [Required]
+     [Required,MaxLength(512)]
      public string Description { get; set; }
      public BaseProduct Article { get; set; }
      public int Count { get; set; }
      public decimal UnitaryCost { get; set; }
+
+     public long EstimateId { get; set; }
+     
+     [JsonIgnore,NotMapped,ForeignKey("EstimateId")]
+     virtual public Estimate Estimate { get; set; }
  }
 
 }
