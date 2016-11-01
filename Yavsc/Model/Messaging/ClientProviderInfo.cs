efc1a80e5b4de12af36ae047bd8a41e11bee7c71
Paul Schneider <paul@pschneider.fr>
2016-11-01 23:22:46 +0100

    wip social

--- Yavsc/Model/Messaging/ClientProviderInfo.cs
+++ Yavsc/Model/Messaging/ClientProviderInfo.cs
@@ -1,14 +1,18 @@
 
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 namespace Yavsc.Model
 {
     public class ClientProviderInfo
     {
         public string UserName { get; set; }
         public string Avatar { get; set; }
+        [Key]
         public string UserId { get; set; }
         public int Rate { get; set; }
         public string EMail { get; set; }
         public string Phone { get; set; }
         public Location BillingAddress { get; set; }
+         public string ChatHubConnectionId { get; set; }
     }
 }
