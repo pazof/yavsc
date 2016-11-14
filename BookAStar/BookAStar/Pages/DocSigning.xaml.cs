e1213442c5d45b9f830400554b0c5957b0b36128
Paul Schneider <paul@pschneider.fr>
2016-11-14 17:55:34 +0100

    WIP signing

---
+++ BookAStar/BookAStar/Pages/DocSigning.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace BookAStar.ViewModels.Signing
+{
+    public partial class DocSigning : ContentPage
+    {
+        public DocSigning()
+        {
+            InitializeComponent();
+        }
+    }
+}
