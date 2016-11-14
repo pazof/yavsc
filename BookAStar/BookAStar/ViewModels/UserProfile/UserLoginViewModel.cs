15a6cfbd969e939b39be0a703601214a7526bce6
Paul Schneider <paul@pschneider.fr>
2016-11-14 17:55:34 +0100

    WIP signing

---
+++ BookAStar/BookAStar/ViewModels/UserProfile/UserLoginViewModel.cs
@@ -0,0 +1,10 @@
+﻿using XLabs.Forms.Mvvm;
+
+namespace BookAStar.ViewModels.UserProfile
+{
+    class UserLoginViewModel : ViewModel
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
