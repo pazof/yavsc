b627544e4fb47f3411efb838a13f2e4901685d06
Paul Schneider <paul@pschneider.fr>
2016-11-30 11:01:50 +0100

    idem

--- Yavsc/ViewModels/Gen/PdfGenerationViewModel.cs
+++ Yavsc/ViewModels/Gen/PdfGenerationViewModel.cs
@@ -1,14 +1,18 @@
 using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNet.Mvc.Rendering;
 
 namespace Yavsc.ViewModels.Gen
 {
     public class PdfGenerationViewModel
     {
+        
         [Required]
         public string TeXSource { get; set; }
         [Required]
         public string BaseFileName { get; set; }
         [Required]
         public string DestDir { get; set; }
+        public bool Generated { get; set; }
+        public HtmlString GenerationErrorMessage { get; set; }
     }
 }
