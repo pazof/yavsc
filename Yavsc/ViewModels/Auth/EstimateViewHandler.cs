d6de99d9a04882ea791658b0b82cbc1872d2662d
Paul Schneider <paul@pschneider.fr>
2016-12-02 13:22:39 +0100

    estimate view authorization

--- Yavsc/ViewModels/Auth/EstimateViewHandler.cs
+++ Yavsc/ViewModels/Auth/EstimateViewHandler.cs
@@ -6,20 +6,17 @@ namespace Yavsc.ViewModels.Auth
 {
     public class EstimateViewHandler : AuthorizationHandler<ViewRequirement, Estimate>
     {
-         protected override void Handle(AuthorizationContext context, ViewRequirement requirement, Estimate resource)
+        protected override void Handle(AuthorizationContext context, ViewRequirement requirement, Estimate resource)
         {
-            if (context.User.IsInRole("Moderator"))
-                context.Succeed(requirement);
-            else if (!context.User.Identity.IsAuthenticated)
-                context.Fail();
-            else {
-            var uid = context.User.GetUserId();
             
-            if (resource.OwnerId == uid || resource.Query.ClientId == uid)
-                context.Succeed(requirement); 
-            else
-            // TODO && ( resource.Circles == null || context.User belongs to resource.Circles )
-                context.Fail();
+            if (context.User.IsInRole(Constants.AdminGroupName)
+            || context.User.IsInRole(Constants.FrontOfficeGroupName))
+                context.Succeed(requirement);
+            else if (context.User.Identity.IsAuthenticated) {
+                var uid = context.User.GetUserId();
+                if (resource.OwnerId == uid || resource.ClientId == uid)
+                    context.Succeed(requirement); 
+                // TODO && ( resource.Circles == null || context.User belongs to resource.Circles )
             }
         }
     }
