a8463ce34401e2585f39749f4203c372f2fe9a3d
Paul Schneider <paul@pschneider.fr>
2016-12-09 13:41:14 +0100

    A reason for queries

--- Yavsc/Migrations/20161209121035_bookQueryReason.Designer.cs
+++ Yavsc/Migrations/20161209121035_bookQueryReason.Designer.cs
@@ -8,9 +8,10 @@ using Yavsc.Models;
 namespace Yavsc.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20161209121035_bookQueryReason")]
+    partial class bookQueryReason
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "7.0.0-rc1-16348");
