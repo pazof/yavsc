61701e21e95264028b2ec87b958e33b463515fe4
Paul Schneider <paul@pschneider.fr>
2016-12-12 14:30:23 +0100

    DIB

---
+++ BookAStar/BookAStar/Pages/Chat/PrivateChatPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace BookAStar.Pages.Chat
+{
+    public partial class PrivateChatPage : ContentPage
+    {
+        public PrivateChatPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
