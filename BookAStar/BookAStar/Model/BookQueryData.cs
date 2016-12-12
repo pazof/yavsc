c1e09b0ca7e0174ee35d90857e61c5bddafdacc2
Paul Schneider <paul@pschneider.fr>
2016-12-12 14:30:15 +0100

    Fix

--- BookAStar/BookAStar/Model/BookQueryData.cs
+++ BookAStar/BookAStar/Model/BookQueryData.cs
@@ -4,12 +4,13 @@ using System;
 
 namespace BookAStar.Model
 {
-    public class BookQueryData : IBookQueryData
+    public class BookQueryData 
     {
         public ClientProviderInfo Client { get; set; }
         public Location Location { get; set; }
         public long Id { get; set; }
         public DateTime EventDate { get; set; }
         public decimal? Previsionnal { get; set; }
+        public string Reason { get; set; }
     }
 }
