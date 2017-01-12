e2c34067a8edc5f11543e95413ca8bea367eba8d
Paul Schneider <paul@pschneider.fr>
2017-01-12 10:12:28 +0100

    Start to implement a FrontOffice index

---
+++ Yavsc/ViewModels/FrontOffice/FrontOfficeIndexViewModel.cs
@@ -0,0 +1,13 @@
+namespace Yavsc.ViewModels.FrontOffice
+{
+    public class FrontOfficeIndexViewModel
+    {
+        public int EstimateToProduceCount { get; set; }
+        public int EstimateToSignAsProCount { get; set; }
+        public int EstimateToSignAsCliCount { get; set; }
+        public int BillToSignAsProCount { get; set; }
+        public int BillToSignAsCliCount { get; set; }
+        public int NewPayementsCount { get; set; }
+
+    }
+}
