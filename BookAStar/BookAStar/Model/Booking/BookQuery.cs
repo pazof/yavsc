c8cac47a560fa38bfb5a75135795eef37c51eee4
Paul Schneider <paul@pschneider.fr>
2017-01-18 13:12:12 +0100

    refactorise

--- BookAStar/BookAStar/Model/Booking/BookQuery.cs
+++ BookAStar/BookAStar/Model/Booking/BookQuery.cs
@@ -5,12 +5,38 @@ namespace BookAStar.Model.Social
 {
     public class BookQuery 
     {
+        /// <summary>
+        /// Client having made this book query.
+        /// </summary>
         public ClientProviderInfo Client { get; set; }
+        /// <summary>
+        /// Location of the client event
+        /// </summary>
         public Location Location { get; set; }
+        /// <summary>
+        /// Unique identifier
+        /// </summary>
         public long Id { get; set; }
+        /// <summary>
+        /// Date of the client event
+        /// </summary>
         public DateTime EventDate { get; set; }
+        /// <summary>
+        /// Amount of money already available, from the site bank, for this event project.
+        /// </summary>
         public decimal? Previsionnal { get; set; }
+        /// <summary>
+        /// Given reason from the client
+        /// </summary>
         public string Reason { get; set; }
+        /// <summary>
+        /// True when the query has been read.
+        /// </summary>
         public bool Read { get; set; }
+        /// <summary>
+        /// Unique identifier of the activity concerned, 
+        /// the context of this query.
+        /// </summary>
+        public string ActivityCode { get; set; }
     }
 }
