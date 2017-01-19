b9220dc3a3d4b512568c59f4a21e5d3bfca7fdf3
Paul Schneider <paul@pschneider.fr>
2017-01-20 00:51:02 +0100

    WIP

---
+++ Yavsc/Models/IT/Modeling/Letter.cs
@@ -0,0 +1,16 @@
+
+using YavscLib;
+
+namespace Yavsc.Models.IT.Modeling
+{
+    public abstract class Letter<T> : ILetter<T>
+    {
+        public abstract bool Equals(T x, T y);
+
+        public int GetHashCode(T obj)
+        {
+            return obj.GetHashCode();
+        }
+
+    }
+}
