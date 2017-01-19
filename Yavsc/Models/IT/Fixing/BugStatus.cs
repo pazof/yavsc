6aadb38df58d20d23b5553ddd5947d95e000c49c
Paul Schneider <paul@pschneider.fr>
2017-01-19 15:30:41 +0100

    IT model

---
+++ Yavsc/Models/IT/Fixing/BugStatus.cs
@@ -0,0 +1,19 @@
+namespace Yavsc.Models.IT.Fixing
+{
+    /// <summary>
+    /// Bug status:
+    /// * Inserted -> Confirmed|FeatureRequest|Feature|Rejected
+    /// * Confirmed -> Fixed
+    /// * FeatureRequest -> Implemented
+    /// </summary>
+    public enum BugStatus : int
+    {
+        Inserted,
+        Confirmed,
+        Rejected,
+        Feature,
+        FeatureRequest,
+        Fixed,
+        Implemented
+    }
+}
