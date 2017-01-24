0f14f683d496f5ac56311109d2821ecde4e1c958
Paul Schneider <paul@pschneider.fr>
2017-01-24 16:15:26 +0100

    cleaning

--- Yavsc/wwwroot/js/md-helpers.js
+++ Yavsc/wwwroot/js/md-helpers.js
@@ -7,25 +7,9 @@ var markdownize = function(content) {
   };
 
 var converter = new showdown.Converter();
-var markdownize = function(content) {
-  if (!content) return '';
-    var html = content.split("\n").map($.trim).filter(function(line) {
-      return line != "";
-    }).join("\n");
-    var md = toMarkdown(html);
-    console.log(md);
-  return md;
-  };
+
  var htmlize = function(content) {
     return converter.makeHtml(content);
-  };
-  var updateHtml = function(id,content) {
-    var jView = jQuery('*[for="'+id+'"]');
-    if (markdownize(jView.html()) === content) {
-      return;
-    }
-    var html = htmlize(content);
-    jView.html(html);
   };
 
   var updateMD = function(id,content) {
@@ -36,7 +20,3 @@ var markdownize = function(content) {
     }
     jQuery('#'+id).val( markdown );
   };
-  var onMDModified = ( function (event, data) {
-    $('#Submit').addClass('dirty');
-    updateMD(this.attributes["for"].value, data.content);
-  });
