a2403b676b6c65a58bde73a7085f21515c8596fe
Paul Schneider <paul@pschneider.fr>
2017-01-26 11:04:03 +0100

    changement de candidat à l'investissement

---
+++ ZicMoove/ZicMoove/Constants.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace BookAStar
+{
+    public static class Constants
+    {
+
+
+#if DEV
+        public const string ApplicationName = "yadev";
+        public const string YavscHomeUrl = "http://dev.pschneider.fr";
+#else
+#if WDEV 
+        public const string ApplicationName = "yawindev";
+        public const string YavscHomeUrl = "http://192.168.0.29:5000";
+#else
+#if YAVSC
+        public const string ApplicationName = "yavsc";
+        public const string YavscHomeUrl = "https://yavsc.pschneider.fr";
+#else
+#if LUA
+        public const string ApplicationName = "Luap";
+        public const string YavscHomeUrl = "https://lua.pschneider.fr";
+#else
+        public const string YavscHomeUrl = "https://booking.pschneider.fr";
+#endif
+#endif
+#endif
+#endif
+        public static readonly string AuthorizeUrl = YavscHomeUrl + "/authorize";
+        public static readonly string RedirectUrl = YavscHomeUrl + "/oauth/success";
+        public static readonly string AccessTokenUrl = YavscHomeUrl + "/token";
+
+        public static readonly string YavscApiUrl = YavscHomeUrl + "/api";
+        public static readonly string MobileRegistrationUrl = YavscApiUrl + "/gcm/register";
+        public static readonly string UserInfoUrl = YavscApiUrl + "/me";
+        public static readonly string BlogUrl = YavscApiUrl + "/blogs";
+        public static readonly string FsUrl = YavscApiUrl + "/fs";
+        public static readonly string SignalRHubsUrl = YavscHomeUrl + "/api/signalr/hubs";
+
+        public static int AllowBeATarget = 1;
+
+        public static int CloudTimeout = 400;
+    }
+}
