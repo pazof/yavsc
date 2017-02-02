8e3cb3029c0a3a1b9408c6d3f4728849972bbf13
Paul Schneider <paul@pschneider.fr>
2017-02-02 12:00:03 +0100

    Sets the default fs user quota to 500M

--- Yavsc/Models/FileSystem/FileRecievedInfo.cs
+++ Yavsc/Models/FileSystem/FileRecievedInfo.cs
@@ -2,6 +2,7 @@ namespace Yavsc.Models.FileSystem
 {
         public class FileRecievedInfo
         {
+            public bool Success { get; set; }
             public string DestDir { get; set; }
             public string FileName { get; set; }
             public bool Overriden { get; set; }
