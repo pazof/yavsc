7ce4d86383f040c1544e4dc65e3543a4dceb9424
Paul Schneider <paul@pschneider.fr>
2017-02-09 00:26:50 +0100

    idem?

--- Yavsc/Controllers/BlogspotController.cs
+++ Yavsc/Controllers/BlogspotController.cs
@@ -106,7 +106,7 @@ namespace Yavsc.Controllers
 
             Blog blog = _context.Blogspot.Include(
                b => b.Author
-           ).Single(m => m.Id == id);
+           ).Include(p => p.ACL).Single(m => m.Id == id);
             if (blog == null)
             {
                 return HttpNotFound();
