11f9714b65e60aa4d1a5c37c8c31bea7acc7cf42
Paul Schneider <paul@pschneider.fr>
2017-02-22 22:58:02 +0100

    signing estimate

--- Yavsc/Interfaces/Workflow/IEvent.cs
+++ Yavsc/Interfaces/Workflow/IEvent.cs
@@ -1,19 +1,22 @@
 
+namespace Yavsc.Interfaces.Workflow {
+    public interface IEvent {
+        /// <summary>
+        /// <c>/topic/(bookquery|estimate)</c>
+        /// </summary>
+        /// <returns></returns>
+        string Topic { get; set ; }
+        /// <summary>
+        /// Should be the user's name
+        /// </summary>
+        /// <returns></returns>
+        string Sender { get; set ; }
+        /// <summary>
+        /// The message 
+        /// </summary>
+        /// <returns></returns>
+        string Message { get; set; }
+    }
 
-public interface IEvent {
-    /// <summary>
-    /// <c>/topic/(bookquery|estimate)</c>
-    /// </summary>
-    /// <returns></returns>
-    string Topic { get; set ; }
-    /// <summary>
-    /// Should be the user's name
-    /// </summary>
-    /// <returns></returns>
-    string Sender { get; set ; }
-    /// <summary>
-    /// The message 
-    /// </summary>
-    /// <returns></returns>
-    string Message { get; set; }
+
 }
