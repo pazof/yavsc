1200f437c2d165271fedbe5c221eafb8925f36ac
Paul Schneider <paul@pschneider.fr>
2017-03-03 00:10:59 +0100

    handle hair cut query

---
+++ ZicMoove/ZicMoove.Droid/Services/GCMHandlers/HairCutQueryGCMHandler.cs
@@ -0,0 +1,21 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using System;
+
+namespace ZicMoove.Droid.Services.GCMHandlers
+{
+    public class HairCutQueryGCMHandler : GCMessageHandler
+    {
+        public HairCutQueryGCMHandler(Context context,
+            NotificationManager notificationManager,
+            Notification.Builder notificationBuilder) : base(context, notificationManager, notificationBuilder)
+        {
+        }
+
+        public override void Handle(string from, Bundle data)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
