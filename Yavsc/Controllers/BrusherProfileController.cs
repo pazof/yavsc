a863aed34cd970a93a4f308aea6a2da2a9584b98
Paul Schneider <paul@pschneider.fr>
2017-03-06 02:04:33 +0100

    refactoring

--- Yavsc/Controllers/BrusherProfileController.cs
+++ Yavsc/Controllers/BrusherProfileController.cs
@@ -1,7 +1,3 @@
-using System.Threading.Tasks;
-using Microsoft.AspNet.Mvc;
-using System.Security.Claims;
-using Microsoft.Data.Entity;
 using Yavsc.Models;
 using Yavsc.Models.Haircut;
 using Microsoft.AspNet.Authorization;
@@ -16,35 +12,6 @@ namespace Yavsc.Controllers
         public BrusherProfileController(ApplicationDbContext context) : base(context)
         {
         }
-        override public async Task<IActionResult> Edit(BrusherProfile brusherProfile)
-        {
-            if (string.IsNullOrEmpty(brusherProfile.UserId))
-            {
-                // a creation
-                brusherProfile.UserId = User.GetUserId();
-                if (ModelState.IsValid)
-                {
-                    _context.BrusherProfile.Add(brusherProfile);
-                    await _context.SaveChangesAsync();
-                    return RedirectToAction("Index");
-                }
-            } 
-            else if (ModelState.IsValid)
-            {
-                _context.Update(brusherProfile);
-                await _context.SaveChangesAsync();
-                return RedirectToAction("Index");
-            }
-            return View(brusherProfile);
-        }
 
-        // POST: BrusherProfile/Delete/5
-        override public async Task<IActionResult> DeleteConfirmed(string id)
-        {
-            BrusherProfile brusherProfile = await _context.BrusherProfile.SingleAsync(m => m.UserId == id);
-            _context.BrusherProfile.Remove(brusherProfile);
-            await _context.SaveChangesAsync();
-            return RedirectToAction("Index");
-        }
     }
 }
--- Yavsc/Controllers/FormationSettingsController.cs
+++ Yavsc/Controllers/FormationSettingsController.cs
@@ -1,120 +1,15 @@
-using System.Threading.Tasks;
-using Microsoft.AspNet.Mvc;
-using Microsoft.Data.Entity;
+using Yavsc.Controllers.Generic;
 using Yavsc.Models;
 using Yavsc.Models.Workflow.Profiles;
 
 namespace Yavsc.Controllers
 {
-    public class FormationSettingsController : Controller
+    public class FormationSettingsController : SettingsController<FormationSettings>
     {
-        private ApplicationDbContext _context;
 
-        public FormationSettingsController(ApplicationDbContext context)
+        public FormationSettingsController(ApplicationDbContext context) : base(context)
         {
-            _context = context;    
         }
 
-        // GET: FormationSettings
-        public async Task<IActionResult> Index()
-        {
-            return View(await _context.FormationSettings.ToListAsync());
-        }
-
-        // GET: FormationSettings/Details/5
-        public async Task<IActionResult> Details(string id)
-        {
-            if (id == null)
-            {
-                return HttpNotFound();
-            }
-
-            FormationSettings formationSettings = await _context.FormationSettings.SingleAsync(m => m.UserId == id);
-            if (formationSettings == null)
-            {
-                return HttpNotFound();
-            }
-
-            return View(formationSettings);
-        }
-
-        // GET: FormationSettings/Create
-        public IActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: FormationSettings/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create(FormationSettings formationSettings)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.FormationSettings.Add(formationSettings);
-                await _context.SaveChangesAsync();
-                return RedirectToAction("Index");
-            }
-            return View(formationSettings);
-        }
-
-        // GET: FormationSettings/Edit/5
-        public async Task<IActionResult> Edit(string id)
-        {
-            if (id == null)
-            {
-                return HttpNotFound();
-            }
-
-            FormationSettings formationSettings = await _context.FormationSettings.SingleAsync(m => m.UserId == id);
-            if (formationSettings == null)
-            {
-                return HttpNotFound();
-            }
-            return View(formationSettings);
-        }
-
-        // POST: FormationSettings/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(FormationSettings formationSettings)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Update(formationSettings);
-                await _context.SaveChangesAsync();
-                return RedirectToAction("Index");
-            }
-            return View(formationSettings);
-        }
-
-        // GET: FormationSettings/Delete/5
-        [ActionName("Delete")]
-        public async Task<IActionResult> Delete(string id)
-        {
-            if (id == null)
-            {
-                return HttpNotFound();
-            }
-
-            FormationSettings formationSettings = await _context.FormationSettings.SingleAsync(m => m.UserId == id);
-            if (formationSettings == null)
-            {
-                return HttpNotFound();
-            }
-
-            return View(formationSettings);
-        }
-
-        // POST: FormationSettings/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(string id)
-        {
-            FormationSettings formationSettings = await _context.FormationSettings.SingleAsync(m => m.UserId == id);
-            _context.FormationSettings.Remove(formationSettings);
-            await _context.SaveChangesAsync();
-            return RedirectToAction("Index");
-        }
     }
 }
