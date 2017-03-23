9bb246e4c0e472b583f1f0b1947e591c92321fdc
Paul Schneider <paul@pschneider.fr>
2017-03-23 02:17:30 +0100

    refabriqué

--- YavscLib/HairCut/IPrestation.cs
+++ YavscLib/HairCut/IPrestation.cs
@@ -4,6 +4,16 @@ namespace YavscLib.HairCut
     {
          long Id { get; set; }
          int Length { get; set; }
+         int Gender { get; set; }
+         bool Cut { get; set; }
 
+         int Dressing { get; set; }
+         int Tech { get; set; }
+
+         bool Shampoo { get; set; }
+
+         long[] Taints { get; set; }
+
+         bool Cares { get; set; }
     }
 }
