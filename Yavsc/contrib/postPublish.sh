b68a5274a03d4113f7f20ac66c256534b7391855
Paul Schneider <paul@pschneider.fr>
2017-04-01 07:06:18 +0200

    [MEF]+script

--- Yavsc/contrib/postPublish.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-ssh root@localhost rm -rf /srv/www/yavscpre/approot/src
-
-(cd bin/output/
-
-rsync -ravu --exclude=.git --chown=www-data:www-data wwwroot approot root@localhost:/srv/www/yavscpre
-ssh root@localhost service kestrel restart
-)
-
