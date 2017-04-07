5881a3662c9326c36d790bce709fc5b67acb9b5d
Paul Schneider <paul@pschneider.fr>
2017-04-08 01:01:01 +0200

    ajoute deux info

--- Yavsc/contrib/rsync-to-pre.sh
+++ Yavsc/contrib/rsync-to-pre.sh
@@ -10,3 +10,7 @@ cd bin/output/
 rsync -ravu wwwroot approot root@localhost:$FSPATH
 ssh root@localhost service kestrel restart
 )
+
+echo "Now, go and try <https://yavscpre.pschneider.fr>"
+echo "Then, feel free to launch contrib/rsync-to-prod.sh"
+
