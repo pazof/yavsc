9b734f2b9ae29588f0af07c85ba6093c4b96b9e2
Paul Schneider <paul@pschneider.fr>
2017-05-26 03:34:09 +0200

    doc

--- Yavsc/Settings/PayPalSettings.cs
+++ Yavsc/Settings/PayPalSettings.cs
@@ -13,18 +13,38 @@ namespace  Yavsc
         ///
         /// </summary>
         public string ClientId { get; set; }
+
         /// <summary>
         /// For sandbox only?
         /// </summary>
         /// <returns></returns>
         public string ClientSecret { get; set; }
+        /// <summary>
+        /// Your application id ...
+        /// </summary>
+        /// <returns></returns>
         public string ApplicationId { get; set; }
+
+        /// <summary>
+        /// Get it from your PayPal Business profile settings
+        /// </summary>
+        /// <returns></returns>
+        public string MerchantAccountId { get; set; }
+
+        /// <summary>
+        /// PayPal Classic Api credentials model
+        /// </summary>
         public class ClassicPayPalAccountApiCredential  {
             public string Signature { get; set; }
             public string ApiUsername { get; set; }
             public string ApiPassword { get; set; }
-
         }
+
+        /// <summary>
+        ///  Live access is configured at:
+        ///  https://www.paypal.com/businessprofile/mytools/apiaccess
+        /// </summary>
         public ClassicPayPalAccountApiCredential[] Accounts { get; set; }
+
     }
 }
