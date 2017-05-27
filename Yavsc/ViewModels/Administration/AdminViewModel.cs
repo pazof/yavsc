724e0e17ef3e158c0f3f7c23cfd0ea0fc9b0dac5
Paul Schneider <paul@pschneider.fr>
2017-05-27 16:22:25 +0200

    ui

--- Yavsc/ViewModels/Administration/AdminViewModel.cs
+++ Yavsc/ViewModels/Administration/AdminViewModel.cs
@@ -1,9 +1,11 @@
 
 namespace Yavsc.ViewModels.Administration {
-    
+
 public class AdminViewModel {
             public RoleInfo [] Roles { get; set; }
             public int  AdminCount { get; set; }
             public bool YouAreAdmin { get; set; }
+
+            public int UserCount { get; set; }
         }
 }
