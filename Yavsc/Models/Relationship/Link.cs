6bf85fcdf5f7caf98ae4e5c0c34484596c0e13b5
Paul Schneider <paul@pschneider.fr>
2017-05-31 01:00:56 +0200

    use links

--- Yavsc/Models/Relationship/Link.cs
+++ Yavsc/Models/Relationship/Link.cs
@@ -1,11 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Yavsc.Models.Relationship
 {
     public class Link
     {
-
+        [Display(Name="Hyper référence")]
         public string HRef { get; set; }
-        public string Rel { get; set; }
+
+        [Display(Name="Methode Http attendue coté serveur")]
         public string Method { get; set; }
+
+        [Display(Name="Classe de lien")]
+        public string Rel { get; set; }
+
+        [Display(Name="Type mime du contenu attendu côté client")]
+        public string ContentType { get; set; }
 
     }
 }
