f8e0f58f024b9d84c6604960aa4ff5b62a4e77da
Paul Schneider <paul@pschneider.fr>
2017-06-08 22:45:34 +0200

    restore l'edition du profile

--- Yavsc/Controllers/FormationSettingsController.cs
+++ Yavsc/Controllers/FormationSettingsController.cs
@@ -1,14 +1,13 @@
 using Yavsc.Controllers.Generic;
 using Yavsc.Models;
 using Yavsc.Models.Workflow.Profiles;
-using Yavsc.Services;
 
 namespace Yavsc.Controllers
 {
     public class FormationSettingsController : SettingsController<FormationSettings>
     {
 
-        public FormationSettingsController(ApplicationDbContext context, IBillingService billing) : base(context, billing)
+        public FormationSettingsController(ApplicationDbContext context) : base(context)
         {
         }
 
