af6060d7ab1b456eb75861128591580311cf3a85
Paul Schneider <paul@pschneider.fr>
2017-08-31 17:36:45 +0200

    [TRY]

--- Yavsc/ApiControllers/Musical/PodcastController.cs
+++ Yavsc/ApiControllers/Musical/PodcastController.cs
@@ -1,22 +1,14 @@
 using System;
 using System.Collections.Concurrent;
 using System.Net.WebSockets;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace Yavsc.ApiControllers
 {
     public class PodcastController : Controller
     {
-        public ArraySegment<Byte> CurrentOutput ;
-        public bool IsEnd;
-
-        ConcurrentBag<WebSocket> Listeners = new ConcurrentBag<WebSocket>();
-
-        public async Task Connect()
-        {
-            var socket = await this.HttpContext.WebSockets.AcceptWebSocketAsync();
-            
-        }
     }
 }
