20df6404debbe31e5dba0d4b11718e351aaa2598
Paul Schneider <paul@pschneider.fr>
2017-09-05 23:19:16 +0200

    seems to be due to mono upgrade ...

--- Yavsc/Controllers/Haircut/BrusherProfileController.cs
+++ Yavsc/Controllers/Haircut/BrusherProfileController.cs
@@ -2,15 +2,16 @@ using Yavsc.Models;
 using Yavsc.Models.Haircut;
 using Microsoft.AspNet.Authorization;
 using Yavsc.Controllers.Generic;
+using Microsoft.Extensions.Localization;
 
 namespace Yavsc.Controllers
 {
     [Authorize(Roles="Performer")]
     public class BrusherProfileController : SettingsController<BrusherProfile>
     {
-
         public BrusherProfileController(ApplicationDbContext context) : base(context)
         {
+
         }
 
     }
