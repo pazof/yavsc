4385af6f3d5fbe10c790b695072374a7b627bade
Paul Schneider <paul@pschneider.fr>
2017-09-20 14:29:06 +0200

    fixe la definition de la fonction

--- Yavsc/wwwroot/js/site.js
+++ Yavsc/wwwroot/js/site.js
@@ -1,9 +1,8 @@
-+(function($) {
-    function notifClick(nid) {
-        if (nid > 0) { 
+var notifClick =
+    function(nid) {
+        if (nid > 0) {
             $.get('/api/dimiss/click/' + nid).done(function() {})
                 .fail(function() {})
                 .always(function() {});
         }
-    }
-})(jQuery);
+    };
