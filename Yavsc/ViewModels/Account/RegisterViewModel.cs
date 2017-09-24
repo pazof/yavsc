2eddfde6ce4888a38a55a638d197f7896bebf850
Paul Schneider <paul@pschneider.fr>
2017-09-24 17:22:25 +0200

    localisation

--- Yavsc/ViewModels/Account/RegisterViewModel.cs
+++ Yavsc/ViewModels/Account/RegisterViewModel.cs
@@ -1,24 +1,32 @@
 using System.ComponentModel.DataAnnotations;
+using Yavsc.Attributes.Validation;
 
 namespace Yavsc.ViewModels.Account
 {
     public class RegisterViewModel
     {
-        [Required][Display(Name = "Nom d'utilisateur")]
+        // ErrorMessage = "",
+         
+
+        [Display(Name = "Nom d'utilisateur")]
+        [StringLength(102)]
+        [YaRegularExpression(@"[a-zA-Z0-9 ._-]+",
+         ErrorMessage = "Caratères autorisés: lettres, chiffres, espace point tiret et souligné.")]
         public string UserName { get; set; }
 
-        [Required]
-        [EmailAddress]
+        [YaRequired("Ce champ est requis.")]
+   //    [EmailAddress]
         [Display(Name = "Email")]
         public string Email { get; set; }
 
-        [Required]
-        [StringLength(100, ErrorMessage = "Le {0} doit être long d'au moins {2} caractères.", MinimumLength = 6)]
-        [DataType(DataType.Password, ErrorMessage="Les mots de passe doivent contenir au moins un caractère spécial, qui ne soit ni une lettre ni un chiffre.")]
+       [YaRequired(ErrorMessage="Spécifiez un mot de passe.")]
+      //  [StringLength(100, ErrorMessage = "Le {0} doit être long d'au moins {1} caractères.", MinimumLength = 6)]
+        [DataType(DataType.Password,
+         ErrorMessage = "Les mots de passe doivent contenir au moins un caractère spécial, qui ne soit ni une lettre ni un chiffre.")]
+        
         [Display(Name = "Mot de passe")]
         public string Password { get; set; }
 
-        [DataType(DataType.Password)]
         [Display(Name = "Confirmer le mot de passe")]
         [Compare("Password", ErrorMessage = "Le mot de passe et sa confirmation ne sont pas les mêmes.")]
         public string ConfirmPassword { get; set; }
