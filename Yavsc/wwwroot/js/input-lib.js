07d3895e6d3557e97be0e51a647767ab4f4bbe48
Paul Schneider <paul@pschneider.fr>
2017-10-22 19:16:14 +0200

    reorg

--- Yavsc/wwwroot/js/input-lib.js
+++ Yavsc/wwwroot/js/input-lib.js
@@ -1,27 +1,22 @@
-+(function($) {
-    var allowCircleToBlog = function(e) {
-        var allow = $(this).prop('checked');
-        var circleid = $(this).data('circle-id');
-        var targetid = $(this).data('target-id');
-        var auth = { CircleId: circleid, BlogPostId: targetid };
-        var url = '/api/blogacl';
-        if (!allow) url += '/' + circleid;
-        console.log(auth);
-        $.ajax({
-            url: url,
-            type: allow ? 'POST' : 'DELETE',
-            data: JSON.stringify(auth),
-            contentType: "application/json;charset=utf-8",
-            success: function(data) {
-                console.log('auth ' + allow ? 'POSTED' : 'DELETED' + data);
-            },
-            error: function() {
-                console.log('Error @' + allow ? 'POSTed' : 'DELETEd');
-            }
-        });
-        e.preventDefault();
-    };
-    $(document).ready(function() {
-        $('input.Blogcirle[type=checkbox]').on('change', allowCircleToBlog);
+var allowCircleToBlog = function(e) {
+    var allow = $(this).prop('checked');
+    var circleid = $(this).data('circle-id');
+    var targetid = $(this).data('target-id');
+    var auth = { CircleId: circleid, BlogPostId: targetid };
+    var url = '/api/blogacl';
+    if (!allow) url += '/' + circleid;
+    console.log(auth);
+    $.ajax({
+        url: url,
+        type: allow ? 'POST' : 'DELETE',
+        data: JSON.stringify(auth),
+        contentType: "application/json;charset=utf-8",
+        success: function(data) {
+            console.log('auth ' + allow ? 'POSTED' : 'DELETED' + data);
+        },
+        error: function() {
+            console.log('Error @' + allow ? 'POSTed' : 'DELETEd');
+        }
     });
-})(jQuery);
+    e.preventDefault();
+}
