33a0185f7ff4795b9315ceb82b9df2c4f853f161
Paul Schneider <paul@pschneider.fr>
2018-02-07 20:17:26 +0100

    fixes latest commit

--- Yavsc.Abstract/Identity/ClientProviderInfo.cs
+++ Yavsc.Abstract/Identity/ClientProviderInfo.cs
@@ -6,9 +6,11 @@ namespace Yavsc.Models.Messaging
     {
         public string UserName { get; set; }
         public string Avatar { get; set; }
+
+        [Key]
         public string UserId { get; set; }
         public string EMail { get; set; }
         public string Phone { get; set; }
-        public ILocation BillingAddress { get; set; }
+        public long BillingAddressId { get; set; }
     }
 }
