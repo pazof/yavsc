0a2330604af77f22a40ec174f4950134ee0bd313
Paul Schneider <paul@pschneider.fr>
2018-04-14 19:35:28 +0200

    a mailling template

---
+++ Yavsc.Server/Models/EMailling/MaillingTemplate.cs
@@ -0,0 +1,65 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Yavsc.Models;
+using Yavsc.Models.Calendar;
+
+namespace Yavsc.Server.Models.EMailling
+{
+    public class MaillingTemplate : IBaseTrackedEntity
+    {
+        /// <summary>
+        /// Date Created
+        /// </summary>
+        /// <returns></returns>
+        public DateTime DateCreated
+        {
+            get;
+            set;
+        }
+
+        public DateTime DateModified
+        {
+            get;
+            set;
+        }
+
+        [Key,DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long Id { get; set; }
+
+        [MaxLengthAttribute(128)]
+        public string ShortName { get; set; }
+
+        /// <summary>
+        /// Markdown template to process
+        /// </summary>
+        /// <returns></returns>
+        [MaxLengthAttribute(64*1024)]
+        public string Body { get; set; }
+
+        [EmailAddress()]
+        public string ReplyToAddress { get; set; }
+
+        [ForeignKey("ManagerId")]
+        public virtual ApplicationUser Manager { get; set; }
+
+        public Periodicity ToSend { get; set; }
+        public string ManagerId
+        {
+            get;
+            set;
+        }
+
+        public string UserCreated
+        {
+            get;
+            set;
+        }
+
+        public string UserModified
+        {
+            get;
+            set;
+        }
+    }
+}
