2a92ad0cac20ce465760c895f442ab752e654c2c
Paul Schneider <paul@pschneider.fr>
2018-04-16 17:56:24 +0200

    init

---
+++ Yavsc/ApiControllers/MailTemplatingApiController.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.AspNet.Mvc;
+using Yavsc.Services;
+
+namespace Yavsc.ApiControllers
+{
+    [Route("api/mailtemplate")]
+    public class MailTemplatingApiController: Controller
+    {
+        
+    }
+}
