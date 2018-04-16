734c9c75d2c016905133890e34473e5b8c8c7e7d
Paul Schneider <paul@pschneider.fr>
2018-04-16 17:53:41 +0200

    cli

---
+++ cli/Services/EMailer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.AspNet.Razor;
+using Microsoft.Extensions.Localization;
+using Microsoft.Extensions.Logging;
+
+namespace cli.Services
+{
+    public class EMailer
+    {
+        RazorTemplateEngine razorEngine;
+        IStringLocalizer<EMailer> stringLocalizer;
+        ILogger logger;
+        public EMailer(RazorTemplateEngine razorTemplateEngine, IStringLocalizer<EMailer> localizer, ILoggerFactory loggerFactory)
+        {
+            logger = loggerFactory.CreateLogger<EMailer>();
+
+            razorEngine = razorTemplateEngine;
+
+            stringLocalizer = localizer;
+        }
+
+        public string Gen(long templateCode)
+        {
+            string subtemp = stringLocalizer["MonthlySubjectTemplate"].Value;
+            logger.LogInformation ( $"Using code: {templateCode} and subject: {subtemp} " );
+            throw new NotImplementedException("razorEngine.GenerateCode");
+        }
+    }
+}
