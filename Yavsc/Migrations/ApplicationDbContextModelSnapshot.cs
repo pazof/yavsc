4b6958e8e99c9979adbedf7dc77fffeb6a60db6b
Paul Schneider <paul@pschneider.fr>
2018-04-18 18:15:40 +0200

    in order to use EF7 commands

--- Yavsc/Migrations/ApplicationDbContextModelSnapshot.cs
+++ Yavsc/Migrations/ApplicationDbContextModelSnapshot.cs
@@ -1,6 +1,8 @@
 using System;
 using Microsoft.Data.Entity;
 using Microsoft.Data.Entity.Infrastructure;
+using Microsoft.Data.Entity.Metadata;
+using Microsoft.Data.Entity.Migrations;
 using Yavsc.Models;
 
 namespace Yavsc.Migrations
