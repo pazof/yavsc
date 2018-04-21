090ca04e131c74457353b1bd6e88ba7442dcb7af
Paul Schneider <paul@pschneider.fr>
2018-04-21 19:30:04 +0200

    mailing [WIP]

--- cli/Services/EMailer.cs
+++ cli/Services/EMailer.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNet.Razor;
 using Microsoft.Extensions.Localization;
 using Microsoft.Extensions.Logging;
+using Yavsc.Models;
 
 namespace cli.Services
 {
@@ -10,13 +11,18 @@ namespace cli.Services
         RazorTemplateEngine razorEngine;
         IStringLocalizer<EMailer> stringLocalizer;
         ILogger logger;
-        public EMailer(RazorTemplateEngine razorTemplateEngine, IStringLocalizer<EMailer> localizer, ILoggerFactory loggerFactory)
+        ApplicationDbContext dbContext;
+
+
+        public EMailer(ApplicationDbContext context, RazorTemplateEngine razorTemplateEngine, IStringLocalizer<EMailer> localizer, ILoggerFactory loggerFactory)
         {
             logger = loggerFactory.CreateLogger<EMailer>();
 
             razorEngine = razorTemplateEngine;
 
             stringLocalizer = localizer;
+
+            dbContext = context;
         }
 
         public string Gen(long templateCode)
