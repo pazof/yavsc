9daaaf39c66da2dc5fed4181d89e908e335018b4
Paul Schneider <paul@pschneider.fr>
2018-04-21 19:29:06 +0200

    a templates controller

---
+++ Yavsc/ApiControllers/MailingTemplateApiController.cs
@@ -0,0 +1,153 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Http;
+using Microsoft.AspNet.Mvc;
+using Microsoft.Data.Entity;
+using Yavsc.Models;
+using Yavsc.Server.Models.EMailing;
+using Microsoft.AspNet.Authorization;
+using System.Security.Claims;
+
+namespace Yavsc.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/mailing")]
+    [Authorize("AdministratorOnly")]
+    public class MailingTemplateApiController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public MailingTemplateApiController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/MailingTemplateApi
+        [HttpGet]
+        public IEnumerable<MailingTemplate> GetMailingTemplate()
+        {
+            return _context.MailingTemplate;
+        }
+
+        // GET: api/MailingTemplateApi/5
+        [HttpGet("{id}", Name = "GetMailingTemplate")]
+        public async Task<IActionResult> GetMailingTemplate([FromRoute] long id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return HttpBadRequest(ModelState);
+            }
+
+            MailingTemplate mailingTemplate = await _context.MailingTemplate.SingleAsync(m => m.Id == id);
+
+            if (mailingTemplate == null)
+            {
+                return HttpNotFound();
+            }
+
+            return Ok(mailingTemplate);
+        }
+
+        // PUT: api/MailingTemplateApi/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutMailingTemplate([FromRoute] long id, [FromBody] MailingTemplate mailingTemplate)
+        {
+            if (!ModelState.IsValid)
+            {
+                return HttpBadRequest(ModelState);
+            }
+
+            if (id != mailingTemplate.Id)
+            {
+                return HttpBadRequest();
+            }
+
+            _context.Entry(mailingTemplate).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!MailingTemplateExists(id))
+                {
+                    return HttpNotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return new HttpStatusCodeResult(StatusCodes.Status204NoContent);
+        }
+
+        // POST: api/MailingTemplateApi
+        [HttpPost]
+        public async Task<IActionResult> PostMailingTemplate([FromBody] MailingTemplate mailingTemplate)
+        {
+            mailingTemplate.ManagerId = User.GetUserId();
+
+            if (!ModelState.IsValid)
+            {
+                return HttpBadRequest(ModelState);
+            }
+
+            _context.MailingTemplate.Add(mailingTemplate);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (MailingTemplateExists(mailingTemplate.Id))
+                {
+                    return new HttpStatusCodeResult(StatusCodes.Status409Conflict);
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("GetMailingTemplate", new { id = mailingTemplate.Id }, mailingTemplate);
+        }
+
+        // DELETE: api/MailingTemplateApi/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteMailingTemplate([FromRoute] long id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return HttpBadRequest(ModelState);
+            }
+
+            MailingTemplate mailingTemplate = await _context.MailingTemplate.SingleAsync(m => m.Id == id);
+            if (mailingTemplate == null)
+            {
+                return HttpNotFound();
+            }
+
+            _context.MailingTemplate.Remove(mailingTemplate);
+            await _context.SaveChangesAsync();
+
+            return Ok(mailingTemplate);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _context.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool MailingTemplateExists(long id)
+        {
+            return _context.MailingTemplate.Count(e => e.Id == id) > 0;
+        }
+    }
+}
---
+++ Yavsc/Controllers/MailingTemplateController.cs
@@ -0,0 +1,152 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Security.Claims;
+using Microsoft.AspNet.Mvc;
+using Microsoft.AspNet.Mvc.Rendering;
+using Microsoft.Data.Entity;
+using Yavsc.Models;
+using Yavsc.Models.Calendar;
+using Yavsc.Server.Models.EMailing;
+using Microsoft.AspNet.Authorization;
+
+
+namespace Yavsc.Controllers
+{
+    [Authorize("AdministratorOnly")]
+    public class MailingTemplateController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public MailingTemplateController(ApplicationDbContext context)
+        {
+            _context = context;    
+        }
+
+        // GET: MailingTemplate
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.MailingTemplate.Include(m => m.Manager);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: MailingTemplate/Details/5
+        public async Task<IActionResult> Details(long? id)
+        {
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+
+            MailingTemplate mailingTemplate = await _context.MailingTemplate.SingleAsync(m => m.Id == id);
+            if (mailingTemplate == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(mailingTemplate);
+        }
+
+
+        List<SelectListItem> GetSelectFromEnum(Type enumType )
+        {
+
+            var list = new List<SelectListItem>();
+            foreach (var v in enumType.GetEnumValues())
+             {
+                 list.Add(new SelectListItem { Value = v.ToString(), Text = enumType.GetEnumName(v) });
+             }
+          return list;
+  
+        }
+
+
+        // GET: MailingTemplate/Create
+        public IActionResult Create()
+        {
+            ViewBag.ManagerId = new SelectList(_context.ApplicationUser, "Id", "UserName");
+            ViewBag.ToSend = GetSelectFromEnum(typeof(Periodicity));
+            return View();
+        }
+
+        // POST: MailingTemplate/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(MailingTemplate mailingTemplate)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.MailingTemplate.Add(mailingTemplate);
+                await _context.SaveChangesAsync(User.GetUserId());
+                return RedirectToAction("Index");
+            }
+            ViewBag.ManagerId = new SelectList(_context.ApplicationUser, "Id", "UserName");
+            ViewBag.ToSend = GetSelectFromEnum(typeof(Periodicity));
+            return View(mailingTemplate);
+        }
+
+        // GET: MailingTemplate/Edit/5
+        public async Task<IActionResult> Edit(long? id)
+        {
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+
+            MailingTemplate mailingTemplate = await _context.MailingTemplate.SingleAsync(m => m.Id == id);
+            if (mailingTemplate == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ManagerId = new SelectList(_context.ApplicationUser, "Id", "UserName");
+            ViewBag.ToSend = GetSelectFromEnum(typeof(Periodicity));
+            return View(mailingTemplate);
+        }
+
+        // POST: MailingTemplate/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(MailingTemplate mailingTemplate)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Update(mailingTemplate);
+                await _context.SaveChangesAsync(User.GetUserId());
+                return RedirectToAction("Index");
+            }
+            ViewBag.ManagerId = new SelectList(_context.ApplicationUser, "Id", "UserName");
+            ViewBag.ToSend = GetSelectFromEnum(typeof(Periodicity));
+            return View(mailingTemplate);
+        }
+
+        // GET: MailingTemplate/Delete/5
+        [ActionName("Delete")]
+        public async Task<IActionResult> Delete(long? id)
+        {
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+
+            MailingTemplate mailingTemplate = await _context.MailingTemplate.SingleAsync(m => m.Id == id);
+            if (mailingTemplate == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(mailingTemplate);
+        }
+
+        // POST: MailingTemplate/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(long id)
+        {
+            MailingTemplate mailingTemplate = await _context.MailingTemplate.SingleAsync(m => m.Id == id);
+            _context.MailingTemplate.Remove(mailingTemplate);
+            await _context.SaveChangesAsync(User.GetUserId());
+            return RedirectToAction("Index");
+        }
+    }
+}
