5e651268bdf2c89b7e4f801fd48e22663bfa7e47
Paul Schneider <paul@pschneider.fr>
2018-05-07 10:11:50 +0200

    PKGFILENAME is to specific for this file

--- versioning.mk
+++ versioning.mk
@@ -1,11 +1,12 @@
 
+ifndef PRJNAME
 PRJNAME := $(shell basename `pwd -P`)
+endif
 SOLUTIONDIR=$(HOME)/workspace/yavsc
 rc_num := $(shell cat $(SOLUTIONDIR)/rc-num.txt)
 DESTDIR=$(SOLUTIONDIR)/build
 MAKE=make
 NUGETSOURCE=$(HOME)/Nupkgs
-PKGFILENAME=$(PRJNAME).$(VERSION).nupkg
 VERSION=1.0.5-rc$(rc_num)
 CONFIGURATION=Release
 
