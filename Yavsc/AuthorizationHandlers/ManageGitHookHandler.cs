bcf46d39422ce2b4493372e06a92f6a347495844
Paul Schneider <paul@pschneider.fr>
2018-06-08 10:25:03 +0200

    [WIP]

---
+++ Yavsc/AuthorizationHandlers/ManageGitHookHandler.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNet.Authorization;
+using System.Security.Claims;
+using Yavsc.Models.IT;
+using Yavsc.Server.Models.IT.SourceCode;
+using Yavsc.ViewModels.Auth;
+
+namespace Yavsc.AuthorizationHandlers
+{
+    public class ManageGitHookHandler: AuthorizationHandler<EditRequirement, GitRepositoryReference>
+    {
+        protected override void Handle(AuthorizationContext context, EditRequirement requirement, GitRepositoryReference resource)
+        {
+            if (context.User.IsInRole("FrontOffice"))
+                context.Succeed(requirement);
+            else if (context.User.Identity.IsAuthenticated)
+                context.Succeed(requirement);
+        }
+
+    }
+}
