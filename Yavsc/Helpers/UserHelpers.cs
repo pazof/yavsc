c8df1f40e8d02a97b0b7b2ce99c06234a1d7907e
Paul Schneider <paul@pschneider.fr>
2018-11-20 15:50:04 +0100

    factorisation

--- Yavsc/Helpers/UserHelpers.cs
+++ Yavsc/Helpers/UserHelpers.cs
@@ -1,5 +1,8 @@
+using System.Collections.Generic;
 using System.Linq;
+using Microsoft.Data.Entity;
 using Yavsc.Models;
+using Yavsc.Models.Blog;
 
 namespace Yavsc.Helpers
 {
@@ -20,6 +23,25 @@ namespace Yavsc.Helpers
             if (user.Avatar==null) return Constants.DefaultAvatar;
             var avatar = user.UserName;
             return $"/Avatars/{avatar}{imgFmt}.png";
+        }
+
+        public static IEnumerable<BlogPost> UserPosts(this ApplicationDbContext dbContext, string posterId, string readerId)
+        {
+            long[] readerCirclesMemberships = dbContext.Circle.Include(c=>c.Members).Where(c=>c.Members.Any(m=>m.MemberId == readerId))
+            .Select(c=>c.Id).ToArray();
+            var result = (readerId!=null)
+            ?
+                 dbContext.Blogspot.Include(
+                 b => b.Author
+                 ).Include(p=>p.ACL).Where(x => x.Author.Id == posterId && 
+                 (x.Visible && 
+                 (x.ACL.Count==0 || x.ACL.Any(a=> readerCirclesMemberships.Contains(a.CircleId)))))
+            :
+                dbContext.Blogspot.Include(
+                b => b.Author
+                ).Where(x => x.Author.Id == posterId && x.Visible);
+                // BlogIndexKey
+            return  result.OrderByDescending(p => p.DateCreated);
         }
     }
 }
