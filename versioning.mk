8543431ebb5f9743e8907331d878245d0904c733
Paul Schneider <paul@pschneider.fr>
2018-12-12 14:43:50 +0000

    prepublishing
    Fixes the destination DIR

--- versioning.mk
+++ versioning.mk
@@ -4,7 +4,6 @@ PRJNAME := $(shell basename `pwd -P`)
 endif
 SOLUTIONDIR=$(HOME)/workspace/yavsc
 rc_num := $(shell cat $(SOLUTIONDIR)/rc-num.txt)
-DESTDIR=$(SOLUTIONDIR)/build
 MAKE=make
 NUGETSOURCE=$(HOME)/Nupkgs
 VERSION=1.0.5-rc$(rc_num)
