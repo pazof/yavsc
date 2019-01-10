e1b692e3a52fc48673856a99276139c5eca43e6a
Paul Schneider <paul@pschneider.fr>
2019-01-10 12:28:04 +0000

    with public properties

--- src/Yavsc.Server/Models/Streaming/LiveFlow.cs
+++ src/Yavsc.Server/Models/Streaming/LiveFlow.cs
@@ -10,19 +10,19 @@ namespace Yavsc.Models.Streaming
       [Key(), DatabaseGenerated(DatabaseGeneratedOption.Identity)]
       [Display(Name="FlowId")]
       // set by the server, unique
-      long Id { get; set; }
+      public long Id { get; set; }
 
       // a title for this flow
-      string Title { get; set; }
+      public string Title { get; set; }
 
       // a little description
-      string Pitch { get; set; }
+      public string Pitch { get; set; }
 
       // The stream type
-      string MediaType { get; set; }
+      public string MediaType { get; set; }
 
       // A name where to save this stream, relative to user's files root
-      string DifferedFileName { get; set; }
+      public string DifferedFileName { get; set; }
 
       [Required]
       public string OwnerId {get; set; }
