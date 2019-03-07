c8321fc72f35f6c1a5d84b7c988905729b5715c6
Paul Schneider <paul@pschneider.fr>
2019-03-07 12:08:17 +0000

    use domain from setting à sending credentials against smtp server

--- src/Yavsc.Server/Settings/SmtpSettings.cs
+++ src/Yavsc.Server/Settings/SmtpSettings.cs
@@ -7,6 +7,7 @@ namespace  Yavsc
         public int Port { get; set; }
         public string UserName { get; set; }
         public string Password { get; set; }
+        public string Domain { get; set; }
 
         public bool EnableSSL { get; set; }
     }
