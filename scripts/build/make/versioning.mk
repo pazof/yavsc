30e26d15af1df7edd6f48feae4e122ffe0d39c89
Paul Schneider <paul@pschneider.fr>
2019-03-17 17:53:25 +0000

    releasing abstract 1.0.6

--- scripts/build/make/versioning.mk
+++ scripts/build/make/versioning.mk
@@ -6,5 +6,5 @@ SOLUTIONDIR=$(HOME)/workspace/yavsc
 rc_num := $(shell cat $(SOLUTIONDIR)/rc-num.txt)
 MAKE=make
 NUGETSOURCE=$(HOME)/Nupkgs
-VERSION=1.0.5-rc$(rc_num)
+VERSION=1.0.6-rc$(rc_num)
 CONFIGURATION=Debug
