0eff78d00ba101a1530401b690f36ceb9c08cd73
Paul Schneider <paul@pschneider.fr>
2019-06-23 00:05:23 +0100

    Localisation

--- src/Yavsc.Server/Models/Messaging/LiveFlow.cs
+++ src/Yavsc.Server/Models/Messaging/LiveFlow.cs
@@ -8,7 +8,7 @@ namespace Yavsc.Models.Streaming
     public partial class LiveFlow : ILiveFlow {
 
       [Key(), DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-      [Display(Name="FlowIdLabel")]
+      [Display(Name="FlowIdLabel", ResourceType=typeof(LiveFlow))]
       // set by the server, unique
       public long Id { get; set; }
 
@@ -18,31 +18,33 @@ namespace Yavsc.Models.Streaming
       /// </summary>
       /// <value></value>
       [StringLength(255)]
-      [Display(Name="TitleLabel")]
+      [Display(Name="TitleLabel", ResourceType=typeof(LiveFlow))]
       public string Title { get; set; }
 
       // a little description
       [StringLength(1023)]
-      [Display(Name="PitchLabel")]
+      [Display(Name="PitchLabel", ResourceType=typeof(LiveFlow))]
       public string Pitch { get; set; }
 
       // The stream type
       [StringLength(127)]
-      [Display(Name="MediaTypeLabel")]
+      [Display(Name="MediaTypeLabel", ResourceType=typeof(LiveFlow))]
       public string MediaType { get; set; }
 
       // A name where to save this stream, relative to user's files root
       [StringLength(255)]
-      [Display(Name="DifferedFileNameLabel")]
+      [Display(Name="DifferedFileNameLabel", ResourceType=typeof(LiveFlow))]
       public string DifferedFileName { get; set; }
+
+      [Display(Name="SequenceNumberLabel", ResourceType=typeof(LiveFlow))]
       public int SequenceNumber { get; set; }
 
       [Required]
-      [Display(Name="OwnerIdLabel")]
+      [Display(Name="OwnerIdLabel", ResourceType=typeof(LiveFlow))]
       public string OwnerId {get; set; }
 
       [ForeignKey("OwnerId")]
-      [Display(Name="OwnerLabel")]
+      [Display(Name="OwnerLabel", ResourceType=typeof(LiveFlow))]
       public virtual ApplicationUser Owner { get; set; }
 
     }
