a37aa4be96c38bcb60af97cec37b83dc5ca8c034
Paul Schneider <paul@pschneider.fr>
2019-06-24 14:53:08 +0100

    fixes this build

--- src/cli/Commands/Builder.cs
+++ src/cli/Commands/Builder.cs
@@ -1,20 +1,24 @@
+using System.IO;
+using Yavsc.Server.Models.IT;
 using Yavsc.Server.Models.IT.SourceCode;
 
 public class Builder {
+    string _gitRepository;
+    private Project _projectInfo;
+
+    public Builder()
+    {
+
+    }
 
 
     public void Clone()
     {
-        var firstProject = _dbContext.Project.Include(p=>p.Repository).FirstOrDefault();
-            Assert.NotNull (firstProject);
-            var di = new DirectoryInfo(_serverFixture.SiteSetup.GitRepository);
-            if (!di.Exists) di.Create();
+        var di = new DirectoryInfo(_gitRepository);
+        if (!di.Exists) di.Create();
 
-
-            var clone = new GitClone(_serverFixture.SiteSetup.GitRepository);
-            clone.Launch(firstProject);
-            gitRepo = di.FullName;
-            
+        var clone = new GitClone(_gitRepository);
+        clone.Launch(_projectInfo);
     }
     
 }
