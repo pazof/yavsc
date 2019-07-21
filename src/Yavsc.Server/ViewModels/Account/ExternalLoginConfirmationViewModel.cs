4013d7f248d050e2a5b4c8de96e84e6b0c11339b
Paul Schneider <paul@pschneider.fr>
2019-07-21 17:35:41 +0200

    Avoid spaces in user names [TODO] fix identity config

--- src/Yavsc.Server/ViewModels/Account/ExternalLoginConfirmationViewModel.cs
+++ src/Yavsc.Server/ViewModels/Account/ExternalLoginConfirmationViewModel.cs
@@ -1,12 +1,14 @@
 
 using System.ComponentModel.DataAnnotations;
-
+using Yavsc.Attributes.Validation;
 
 namespace Yavsc.ViewModels.Account
 {
     public class ExternalLoginConfirmationViewModel
     {
         [Required]
+        [YaStringLength(2,Constants.MaxUserNameLength)]
+        [YaRegularExpression(Constants.UserNameRegExp)]
         public string Name { get; set; }
 
         [Required]
