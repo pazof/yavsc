42cf9403cfdc210e85d2189de67c1b7ded7cefa4
Paul Schneider <paul@pschneider.fr>
2019-08-03 11:45:31 +0200

    refact & version file name

--- scripts/build/make/versioning.mk
+++ scripts/build/make/versioning.mk
@@ -8,3 +8,12 @@ MAKE=make
 NUGETSOURCE=$(HOME)/Nupkgs
 VERSION=$(version)
 CONFIGURATION=Debug
+
+version-check:
+ifndef version
+	@echo no version number specification ... please, could you try and run 'echo 1.2.3 > $(SOLUTIONDIR)version.txt' ?. 
+else
+	@echo 'Got version number : $(version)'
+endif
+
+
