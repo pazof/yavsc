40c9c9bba948cb05518170ff2a4e18e8726fae25
Paul Schneider <paul@pschneider.fr>
2019-08-05 02:22:11 +0200

    refact + owner required for a circle

--- src/Yavsc.Abstract/Interfaces/Workflow/IBillingClause.cs
+++ src/Yavsc.Abstract/Interfaces/Workflow/IBillingClause.cs
@@ -4,6 +4,10 @@ namespace Yavsc.Models.Billing {
     public interface IBillingClause { 
         string Description {get; set;}
         IBillingImpacter Impacter { get; }
+
+        // TODO
+        // Conditions de ventes relatives à l'impact
+        // IEnumerable<long> CGV,CPV 
     }
 
 }
