2ee66b4a01579afd306b6fa5c6e35c4a4e2b575b
Paul Schneider <paul@pschneider.fr>
2019-09-05 15:16:28 +0100

    linting js code

--- src/Yavsc/wwwroot/js/md-helpers.js
+++ src/Yavsc/wwwroot/js/md-helpers.js
@@ -1,22 +1,22 @@
 var markdownize = function(content) {
   if (!content) return '';
-    var html = content.split("\n").map($.trim).filter(function(line) {
-      return line != "";
-    }).join("\n");
+    var html = content.split('\n').map($.trim).filter(function(line) {
+      return line != '';
+    }).join('\n');
   return toMarkdown(html);
   };
 
 var converter = new showdown.Converter();
 
- var htmlize = function(content) {
+var htmlize = function(content) {
     return converter.makeHtml(content);
   };
 
-  var updateMD = function(id,content) {
-  if (!content) return jQuery('#'+id).val('') ;
+var updateMD = function(id,content) {
+  if (!content) return jQuery('#' + id).val('');
     var markdown = markdownize(content);
-    if (jQuery('#'+id).val() === markdown) {
+    if (jQuery('#' + id).val() === markdown) {
       return;
     }
-    jQuery('#'+id).val( markdown );
+    jQuery('#' + id).val(markdown);
   };
