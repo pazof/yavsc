454d32bdaaa35796d0161a5337c7c04a94f9f2ad
Paul Schneider <paul@pschneider.fr>
2019-09-08 01:35:31 +0100

    renamed

---
+++ src/Yavsc/wwwroot/js/audiovideoinput.js
@@ -0,0 +1,13 @@
+
+var constraints = { audio: true, video: false }
+
+navigator.mediaDevices.getUserMedia(constraints)
+    .then(function(stream) {
+        /* use the stream */
+        console.log("got stream!");
+        console.log(stream)
+    })
+    .catch(function(err) {
+        /* handle the error */
+        console.log(err)
+    });
