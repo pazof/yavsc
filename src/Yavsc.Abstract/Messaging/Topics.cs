5178629314b0233c54cfd2aead34212b100e448e
Paul Schneider <paul@pschneider.fr>
2019-09-17 11:37:36 +0100

    renaming topic constants

--- src/Yavsc.Abstract/Messaging/Topics.cs
+++ src/Yavsc.Abstract/Messaging/Topics.cs
@@ -1,9 +1,10 @@
 namespace Yavsc.Abstract.Messaging
 {
-    public static class MessagingConstants {
-        public static readonly string TopicGeneral = "/topic/general";
-        public static readonly string TopicRdvQuery = "/topic/RdvQuery";
-        public static readonly string TopicEstimation = "/topic/Estimation";
-        public static readonly string TopicHairCutQuery = "/topic/HairCutQuery";
+    public static class Topics {
+        public static readonly string General = "/topic/general";
+        public static readonly string RdvQuery = "/topic/RdvQuery";
+        public static readonly string Estimation = "/topic/Estimation";
+        public static readonly string HairCutQuery = "/topic/HairCutQuery";
+
     }  
 }
