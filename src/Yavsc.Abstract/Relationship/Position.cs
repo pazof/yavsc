4fbd2015a7cc68c7796312758cc63a147eb2da4c
Paul Schneider <paul@pschneider.fr>
2019-09-18 01:38:46 +0100

    allow RdvQueryProviderInfo to be deserialised

--- src/Yavsc.Abstract/Relationship/Position.cs
+++ src/Yavsc.Abstract/Relationship/Position.cs
@@ -1,10 +1,8 @@
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 using Yavsc.Attributes.Validation;
 
 namespace Yavsc.Models.Relationship
 {
-
     /// <summary>
     /// Position.
     /// </summary>
@@ -27,13 +25,4 @@ namespace Yavsc.Models.Relationship
 		public double Latitude { get; set; }
 
 	}
-
-    public class Location : Position, ILocation {
-        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public long Id { get; set; }
-        [YaRequired(),
-        Display(Name="Address"),
-        MaxLength(512)]
-        public string Address { get; set; }
-    }
 }
