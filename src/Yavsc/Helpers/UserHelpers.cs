766535cde952b2386d35ce613aad95730f0ddcba
Paul Schneider <paul@pschneider.fr>
2019-09-27 03:21:35 +0100

    removing avatar component

--- src/Yavsc/Helpers/UserHelpers.cs
+++ src/Yavsc/Helpers/UserHelpers.cs
@@ -8,22 +8,6 @@ namespace Yavsc.Helpers
 {
     public static class UserHelpers
     {
-        /// <summary>
-        /// The avatar ...
-        /// </summary>
-        /// <param name="dbContext"></param>
-        /// <param name="userId"></param>
-        /// <param name="imgFmt"></param>
-        /// <returns></returns>
-        // FIXME support imgFmt
-        public static string AvatarUri(this ApplicationDbContext dbContext, string userId, string imgFmt )
-        {
-            var user = dbContext.Users.FirstOrDefault(u => u.Id == userId);
-            if (user==null) return Constants.AnonAvatar;
-            if (user.Avatar==null) return Constants.DefaultAvatar;
-            var avatar = user.UserName;
-            return $"/Avatars/{avatar}{imgFmt}.png";
-        }
 
         public static IEnumerable<BlogPost> UserPosts(this ApplicationDbContext dbContext, string posterId, string readerId)
         {
