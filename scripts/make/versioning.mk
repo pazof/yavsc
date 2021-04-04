3fe3e23c8e692d0a626e3e0a6be78c3292f297fe
Paul Schneider <paulschneider@free.fr>
2021-04-04 22:20:46 +0100

    hotfix solution dir

--- scripts/make/versioning.mk
+++ scripts/make/versioning.mk
@@ -2,8 +2,7 @@
 ifndef PRJNAME
 PRJNAME := $(shell basename `pwd -P`)
 endif
-SOLUTIONDIR=../..
-version := $(shell cat $(SOLUTIONDIR)/version.txt)
+version := $(shell cat ../../version.txt)
 MAKE=make
 NUGETSOURCE=$(HOME)/Nupkgs
 VERSION=$(version)
@@ -11,7 +10,7 @@ CONFIGURATION=Debug
 
 version-check:
 ifndef version
-	@echo no version number specification ... please, could you try and run 'echo 1.2.3 > $(SOLUTIONDIR)version.txt' ?. 
+	@echo no version number specification ... please, could you try and run 'echo 1.2.3 >  ../../version.txt' ?. 
 else
 	@echo 'Got version number : $(version)'
 endif
