9bbbb22860ee4f8f5995924d3a5e5a4fefbc91b8
Paul Schneider <paulschneider@free.fr>
2023-03-27 22:45:16 +0100

    a layout

--- src/Yavsc/wwwroot/js/site.js
+++ src/Yavsc/wwwroot/js/site.js
@@ -2,3 +2,14 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+var notifClick =
+    function(nid) {
+        if (nid > 0) {
+            $.get('/api/dimiss/click/' + nid);
+        }
+    };
+
+var setUiCult = function(lngspec) {
+    document.cookie = 'ASPNET_CULTURE=c=' + lngspec + '|uic=' + lngspec;
+    location.reload();
+};
