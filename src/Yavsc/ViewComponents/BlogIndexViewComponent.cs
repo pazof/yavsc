3495523bd4a6188e470e6ba7b741d98f055c7127
Paul Schneider <paulschneider@free.fr>
2025-02-18 20:17:06 +0000

    Blog posts Permission handling

--- src/Yavsc/ViewComponents/BlogIndexViewComponent.cs
+++ src/Yavsc/ViewComponents/BlogIndexViewComponent.cs
@@ -3,6 +3,9 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Yavsc.Models;
 using Yavsc.Models.Blog;
+using Yavsc.Helpers;
+using System.Security.Claims;
+using IdentityServer8.Extensions;
 
 namespace Yavsc.ViewComponents
 {
@@ -18,23 +21,39 @@ namespace Yavsc.ViewComponents
 
         // Renders blog index ofr the specified user by name,
         // grouped by title
-        public async Task<IViewComponentResult> InvokeAsync(string viewerId, int skip=0, int maxLen=25)
+        public async Task<IViewComponentResult> InvokeAsync(int skip=0, int maxLen=25)
         {
-            long[] usercircles = await _context.Circle.Include(c=>c.Members).
-            Where(c=>c.Members.Any(m=>m.MemberId == viewerId))
-            .Select(c=>c.Id).ToArrayAsync();
+            IEnumerable<BlogPost> posts;
 
-            var allposts = _context.BlogSpot
+            if (User.IsAuthenticated())
+            {
+                string viewerId = UserClaimsPrincipal.GetUserId();
+                long[] usercircles = await _context.Circle.Include(c=>c.Members).
+                    Where(c=>c.Members.Any(m=>m.MemberId == viewerId))
+                    .Select(c=>c.Id).ToArrayAsync();
+                
+                IQueryable<BlogPost> allposts = _context.BlogSpot
+                    .Include(b => b.Author)
+                    .Include(p=>p.ACL)
+                    .Include(p=>p.Tags)
+                    .Include(p=>p.Comments)
+                    .Where(p => p.AuthorId == viewerId || p.Visible);
+
+                posts = (usercircles != null) ? 
+                    allposts.Where(p=> p.ACL.Count==0 || p.ACL.Any(a => usercircles.Contains(a.CircleId)))
+                    : allposts.Where(p => p.ACL.Count == 0);
+
+            }
+            else 
+            {
+                 posts = _context.BlogSpot
                 .Include(b => b.Author)
                 .Include(p=>p.ACL)
                 .Include(p=>p.Tags)
                 .Include(p=>p.Comments)
-                .Where(p => p.AuthorId == viewerId || p.Visible).ToArray();
-
-            IEnumerable<BlogPost> posts = (usercircles != null) ? 
-                allposts.Where(p=> p.ACL.Count==0 || p.ACL.Any(a => usercircles.Contains(a.CircleId)))
-                : allposts.Where(p => p.ACL.Count == 0);
-
+                .Where(p => p.Visible && p.ACL.Count == 0 ).ToArray();
+            }
+          
             var data = posts.OrderByDescending( p=> p.DateCreated);
             var grouped = data.GroupBy(p=> p.Title).Skip(skip).Take(maxLen);
 
