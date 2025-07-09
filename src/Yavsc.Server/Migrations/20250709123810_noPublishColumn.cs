6f4f5f94b6ee0675c6774f71f93f7d1dde0d1566
Paul Schneider <paulschneider@free.fr>
2025-07-09 13:38:36 +0100

    noPublishColumn

---
+++ src/Yavsc.Server/Migrations/20250709123810_noPublishColumn.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Yavsc.Migrations
+{
+    /// <inheritdoc />
+    public partial class noPublishColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Publish",
+                table: "BlogSpot");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Publish",
+                table: "BlogSpot",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
