34ef8bedfff58b952f5199b6752d3b55868ed326
Paul Schneider <paul@pschneider.fr>
2026-06-06 21:59:00 +0100

    scopes

--- src/Yavsc.Org/Contants.cs
+++ src/Yavsc.Org/Contants.cs
@@ -1,5 +1,5 @@
 
 public static class Constants
 {
-     public static readonly string[] BuildInApiScopes = {  "blog", "admin", "contract", "com" };
+     public static readonly string[] BuildInApiScopes = {  "blog", "admin", "moderation", "performer", "client" };
 }
