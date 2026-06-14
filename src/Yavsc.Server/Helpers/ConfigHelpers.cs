aabbbc1fea15ffadae1dc49f24bc2aae298fd9cf
Paul Schneider <paul@pschneider.fr>
2026-06-15 00:11:48 +0100

    use the helper

--- src/Yavsc.Server/Helpers/ConfigHelpers.cs
+++ src/Yavsc.Server/Helpers/ConfigHelpers.cs
@@ -8,11 +8,10 @@ public static class ConfigHelpers
     public static IConfigurationBuilder AddConfiguration(this WebApplicationBuilder builder, string appName )
         {
             var rootConfig = builder.Configuration
-                            .AddJsonFile($"appsettings-{appName}.json", optional: false, reloadOnChange: false);
+                 .AddJsonFile($"appsettings-{appName}.json", optional: false, reloadOnChange: false);
 
-            if (!String.IsNullOrWhiteSpace(builder.Environment.EnvironmentName))
-                rootConfig.AddJsonFile($"appsettings-{appName}.{builder.Environment.EnvironmentName}.json", optional: true, reloadOnChange: false);
-            ;
+            rootConfig.AddJsonFile($"appsettings-{appName}.{builder.Environment.EnvironmentName}.json", optional: true, reloadOnChange: false);
+            
             return rootConfig.AddEnvironmentVariables();
         }
 }
