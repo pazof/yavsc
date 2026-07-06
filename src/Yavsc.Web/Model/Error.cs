fcb93d41d2817513b5331e52eb19e92b8d940647
Paul Schneider <paul@pschneider.fr>
2026-07-06 03:31:05 +0100

    code cleanup

--- src/Yavsc.Web/Model/Error.cs
+++
@@ -1,8 +0,0 @@
-using System;
-
-public class ErrorViewModel
-{
-    public string RequestId { get; set; } = String.Empty;
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
--- src/templateWeb/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace templateWeb.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
